dd7ec6bafee226de4df41ed1ebd0f14a37ec017f
ermaotech <ermaotech@163.com>
2020-07-08 13:21:05 +0800

    新增二叉树最大深度

--- 104. Maximum Depth of Binary Tree.cpp
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -5,27 +5,21 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
+        // 当节点为NULL时，深度为0
         if (root == nullptr) return 0;
-
-        if (root->left == nullptr && root->right == nullptr)
-            return 1;
         
+        // 当节点的左右子节点都为NULL时，说明只有一个节点，深度为1
+        // 不过这块貌似可以优化掉，让递归到第一个if条件去
+        // if (root->left == nullptr && root->right == nullptr)
+        //     return 1;
+        
+        // 获取左子树深度
         int leftDepth = maxDepth(root->left);
+        // 获取右子树深度
         int rightDepth = maxDepth(root->right);
 
+        // 根据左右子树深度，返回最大的那个深度+1，+1是加上当前节点
         return leftDepth > rightDepth ? leftDepth + 1; rightDepth + 1;
-    }
-
-    int getMaxDepth(TreeNode* root, int deep) {
-        if (root == nullptr) return 0;
-
-        if (root->left == nullptr && root->right == nullptr)
-            return 1;
-        
-        int leftDepth = getMaxDepth(root->left);
-        int rightDepth = getMaxDepth(root->right);
-
-        return leftDepth > rightDepth ? leftDepth + 1: rightDepth + 1;
     }
 };
 
